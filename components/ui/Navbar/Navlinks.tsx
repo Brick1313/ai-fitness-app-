3ddfaee67d87810eb3c5021d4c09179a3359b269
Brick1313 <chiro@drdennisbrick.com>
2026-01-26 23:49:12 -1000

    Update Navlinks.tsx

--- components/ui/Navbar/Navlinks.tsx
+++ components/ui/Navbar/Navlinks.tsx
@@ -36,7 +36,7 @@ export default function Navlinks({ user }: NavlinksProps) {
       <div className="flex justify-end space-x-8">
         {user ? (
           <form onSubmit={(e) => handleRequest(e, SignOut, router)}>
-            <input type="hidden" name="pathName" value={pathname} />
+            <input type="hidden" name="pathName" value={pathname || "/"} />
             <button type="submit" className={s.link}>
               Sign out
             </button>

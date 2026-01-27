64b0b5ad7160cf4390736c1ebca486271a668c15
Brick1313 <chiro@drdennisbrick.com>
2026-01-27 10:25:30 -1000

    Update Pricing.tsx

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -56,7 +56,7 @@ export default function Pricing({ user, products, subscription }: Props) {
 
     const { errorRedirect, sessionId } = await checkoutWithStripe(
       price,
-      currentPath
+      currentPath || undefined
     );
 
     if (errorRedirect) {
